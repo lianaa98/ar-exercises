534e24d846eef90191f07074e2465c89a3e63337
lianaa98 <liana.kan98@gmail.com>
2023-05-29 15:39:27 -0700

    complete ex2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+store = Store.find_by("id": 1)
+@store1 = store
+store = Store.find_by("id": 2)
+@store2 = store
+
+# Update the first store instance in db
+@store1.update(name: "STORE_BURNABY")
