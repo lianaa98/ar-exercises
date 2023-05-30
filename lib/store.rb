b2ae35f845cc07424bd2bdf575d0abb9b9e4c4c9
lianaa98 <liana.kan98@gmail.com>
2023-05-30 11:22:20 -0700

    update models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "The store must carry at least one of men's or women's apparel")
+    end
+  end
 
 end
