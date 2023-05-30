dfe92bea6e7c6a885a79a9cc07f4d69c74503248
lianaa98 <liana.kan98@gmail.com>
2023-05-30 11:19:55 -0700

    complete ex7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name:"
+print "> "
+user_input = gets.chomp
+puts "Creating store..."
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+store = Store.create(name: user_input)
+store.save
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+if store.save
+  puts "Store created!"
+else
+  puts store.errors.full_messages
+end
