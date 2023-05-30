bc61d2b97cc0ab3db25e216a42ffb245304ece37
lianaa98 <liana.kan98@gmail.com>
2023-05-30 00:41:47 -0700

    complete ex4, append queries

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -15,3 +15,17 @@ stores = [
 
 Store.create(stores)
 puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+for store in @mens_stores
+  puts "#{store.name}, #{store.annual_revenue}"
+end
+
+@womens_stores_less_than_one_mil = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+
+for store in @womens_stores_less_than_one_mil
+  puts "#{store.name}, #{store.annual_revenue}"
+end
+
+
