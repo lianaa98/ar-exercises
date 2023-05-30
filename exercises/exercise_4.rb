923a34d240e96ffaab0910ef4710cf1eac2ac336
lianaa98 <liana.kan98@gmail.com>
2023-05-30 00:29:22 -0700

    complete ex4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,11 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+stores = [
+  {name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true},
+  {name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false},
+  {name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true},
+]
+
+Store.create(stores)
+puts Store.count
